37a8d8e815948f548f0801bfcd84a6f0ada71052
TejaswiniRadha <teju.nama@gmail.com>
2022-01-05 15:50:51 +0530

    1 file modified, list add and remove items in list

--- src/App.js
+++ src/App.js
@@ -4,19 +4,50 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: 'tejaswini',
+      name: '',
+      names: [],
     };
   }
-  parenthandler = (childdata) => {
-    this.setState({ name: childdata });
-    console.log(childdata);
+  changehandler = (e) => {
+    this.setState({ name: e.target.value });
+    //console.log(e.target.value);
+  };
+  submithandler = () => {
+    const { names } = this.state;
+    names.push(this.state.name);
+    this.setState({ names, name: '' });
+    //console.log(names);
+  };
+  removehandler = (index) => {
+    let { names } = this.state;
+    names.splice(index, 1);
+    this.setState({ names });
+    console.log(names);
   };
   render() {
     return (
       <div>
         <h1>parent component</h1>
-        name:{this.state.name}
-        <Child parenthandler={this.parenthandler} />
+        <input
+          type="text"
+          placeholder="username"
+          name="name"
+          value={this.state.name}
+          onChange={this.changehandler}
+        />
+        <button onClick={this.submithandler}>submit</button>
+        <ul>
+          {this.state.names.map((name, index) => (
+            <li
+              onClick={() => {
+                this.removehandler(index);
+              }}
+              key={index}
+            >
+              {name}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
